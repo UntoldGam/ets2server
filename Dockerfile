fdabef34e5efac21635340500659b6f26ad2f7d5
UntoldGam <charlessbird+github@outlook.com>
2024-11-05 12:11:05 +0000

    update

--- Dockerfile
+++ Dockerfile
@@ -4,19 +4,19 @@ ENV DEBIAN_FRONTEND=noninteractive
 ENV STEAMCMD_DIR=/steamcmd
 ENV ETS2_SERVER_DIR=/ets2
 
-# Install dependencies
+# Install QEMU and related tools
 RUN apt-get update && apt-get install -y --no-install-recommends \
     qemu-user \
     qemu-user-static \
     binfmt-support \
-    libc6:i386 \
-    libcurl4:i386 \
-    libssl1.1:i386 \
-    steamcmd \
-    && rm -rf /var/lib/apt/lists/*
+    curl \
+    ca-certificates \
+    && apt-get clean && rm -rf /var/lib/apt/lists/*
 
-# Create necessary directories
-RUN mkdir -p ${STEAMCMD_DIR} ${ETS2_SERVER_DIR}
+# Install SteamCMD manually (x86 binaries will run under QEMU)
+RUN mkdir -p ${STEAMCMD_DIR} && \
+    cd ${STEAMCMD_DIR} && \
+    curl -s https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz | tar -xz
 
 # Expose ports
 EXPOSE 27015/udp
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 set -e
 
-# Set default values for environment variables if not set
-STEAM_USER=${STEAM_USER:-anonymous}
-STEAM_PASS=${STEAM_PASS:-""}
-SERVER_TOKEN=${SERVER_TOKEN}
-MAX_PLAYERS=${MAX_PLAYERS:-8}
-SERVER_NAME=${SERVER_NAME:-"ETS2 Dedicated Server"}
-SERVER_DESCRIPTION=${SERVER_DESCRIPTION:-"Dedicated ETS2 Server"}
-SERVER_PASSWORD=${SERVER_PASSWORD:-""}
-
-# Download or update the ETS2 server
-${STEAMCMD_DIR}/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} \
+# Run SteamCMD under QEMU
+/usr/bin/qemu-x86_64-static ${STEAMCMD_DIR}/steamcmd.sh +login anonymous \
     +force_install_dir ${ETS2_SERVER_DIR} \
-    +app_update 1948160 validate \
-    +quit
+    +app_update 1948160 validate +quit
 
-# Start the server
-cd ${ETS2_SERVER_DIR}
-./bin/linux_x64/eurotrucks2_server \
+# Start the ETS2 server
+${ETS2_SERVER_DIR}/bin/linux_x64/eurotrucks2_server \
     -server \
     -server_auth_token ${SERVER_TOKEN} \
     -server_max_playerrs ${MAX_PLAYERS} \
